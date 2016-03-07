e88291cfe7559f1ac9cfc3478b1cd30ea1bee261
James Kiesel <james.kiesel@gmail.com>
2016-03-07 22:27:55 +1300

    Part III: Create an API endpoint

---
+++ controllers/discussion_tags_controller.rb
@@ -0,0 +1,14 @@
+class ::API::DiscussionTagsController < API::RestfulController
+  def index
+    instantiate_collection { |collection| collection.where('discussions.key': params[:discussion_keys]) }
+    respond_with_collection
+  end
+
+  private
+
+  def accessible_records
+    resource_class.joins(:discussion)
+                  .includes(:tags)
+                  .where('discussions.id': Queries::VisibleDiscussions.new(user: current_user).pluck(:id))
+  end
+end
